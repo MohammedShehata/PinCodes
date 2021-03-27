ab05c5efb62da64752eb729aa54acda8b9de0e2a
Mohammed Shehata <shehata.mohammed@gmail.com>
2021-03-27 21:33:16 +0200

    ADD pin codes sample ui

--- sample/src/main/java/com/sh/pincodes/sample/MainActivity.kt
+++ sample/src/main/java/com/sh/pincodes/sample/MainActivity.kt
@@ -1,11 +1,40 @@
 package com.sh.pincodes.sample
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.sh.pincodes.PinCodes
+import com.sh.pincodes.sample.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), PinCodes.OnPinsCompleteListener {
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        PinCodes.newInstance(
+            this,
+            binding.editPin1,
+            binding.editPin2,
+            binding.editPin3,
+            binding.editPin4,
+            binding.editPin5,
+            binding.editPin6
+        )
+
+        PinCodes.newInstance(
+            this,
+            binding.editPin21,
+            binding.editPin22,
+            binding.editPin23,
+            binding.editPin24,
+        )
+    }
+
+    override fun onPinsCompleted() {
+        Toast.makeText(this, "pins group completed", Toast.LENGTH_SHORT).show()
     }
 }
